b63c33bbcc68787d1289b22cca097cd9b0aedefb
Joujirou, Tani <joujirou.tani@sonymobile.com>
2014-10-23 21:30:14 +0900

    Add a wait for creating crypt block device
    Creating the crypt block device takes some time,
    especially on low performance platform.
    Currently vold isn't aware of it so sometimes vold
    tries to access it before the creation is completed.
    In that case the encryption or decryption would fail.
    To avoid this problem, the wait for creating the block
    device is added.
    
    Change-Id: I3dd24ffc0257d5b41505cb5c27f8362aea10ec14

--- cryptfs.c
+++ cryptfs.c
@@ -1158,6 +1158,8 @@ static int create_crypto_blk_dev(struct crypt_mnt_ftr *crypt_ftr,
   int version[3];
   char *extra_params;
   int load_count;
+  struct stat statbuf;
+  int retry = 25;
 
   if ((fd = open("/dev/device-mapper", O_RDWR|O_CLOEXEC)) < 0 ) {
     SLOGE("Cannot open device-mapper\n");
@@ -1206,6 +1208,16 @@ static int create_crypto_blk_dev(struct crypt_mnt_ftr *crypt_ftr,
   if (ioctl(fd, DM_DEV_SUSPEND, io)) {
     SLOGE("Cannot resume the dm-crypt device\n");
     goto errout;
+  }
+
+  /* wait for creating dm device */
+  while (stat(crypto_blk_name, &statbuf) != 0) {
+      retry--;
+      if (retry < 0) {
+          SLOGE("Timed out to getting dm blkdev status.\n");
+          goto errout;
+      }
+      usleep(40 * 1000);
   }
 
   /* We made it here with no errors.  Woot! */
